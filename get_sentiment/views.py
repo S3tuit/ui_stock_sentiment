a1518a68938f8b63f91424d9a539c4c9b2504229
Matteo Ladislai <maladislai@paa.it>
2024-09-16 21:47:40 +0200

    ADDED OPENAI STRUCTURED OUTPUT

--- get_sentiment/views.py
+++ get_sentiment/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from pymongo import MongoClient
 from openai import OpenAI
+from pydantic import BaseModel
 
 TICKETS = {'RIOT', 'ACMR'}
 
+class StockSentiment(BaseModel):
+    score: int
+    reasoning: str
+    ticket: str
 
 
 def get_articles(ticket):
@@ -19,32 +24,31 @@ def get_articles(ticket):
         return articles
 
 
-def get_sentiment(articles, ticket):
+def get_sentiment(articles, ticket, format):
     openai_message = f'''
     Below, you'll find articles about the stock {ticket}. Analyze them and give a score to the stock sentiment from 1 to 100.
-    Write a short reasoning explaining the why of your score. Here are the articles: \n\n
+    Write a comprehensive and thorough reasoning explaining the why of your score. Here are the articles: \n\n
     '''
 
     for article in articles:
         openai_message += article['article_body'] + '\n\n'
     client = OpenAI()
     
-    completion = client.chat.completions.create(
+    completion = client.beta.chat.completions.parse(
         model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": "You're now the best stock analyzer."},
-            {
-                "role": "user",
-                "content": openai_message
-            }
-        ]
+            {"role": "user", "content": openai_message}
+        ],
+        response_format=format
     )
         
-    return completion.choices[0].message.content
+    return completion.choices[0].message.parsed
     
 
 def home_view(request, *args, **kwargs):
     sentiment = None
+    error_msg = None
     if request.method == 'POST':
         user_input = request.POST.get('user_input')
         
@@ -52,19 +56,21 @@ def home_view(request, *args, **kwargs):
             ticket = user_input.upper()
             articles = get_articles(ticket)
             
-            sentiment = get_sentiment(articles, ticket)
+            sentiment = get_sentiment(articles, ticket, StockSentiment)
+            sentiment.ticket = ticket
         
             # Store the sentiment in session
-            request.session['user_input'] = sentiment
+            request.session['user_input'] = sentiment.__dict__
             
             # Redirect to the same view to prevent re-submission
             return redirect('home')
         else:
-            sentiment = 'ERROR :('
-            request.session['user_input'] = sentiment
+            error_msg = 'The ticket is not present in our database yet :('
+            request.session['user_input_error'] = error_msg
     
             return redirect('home')
     
     sentiment = request.session.pop('user_input', None)
+    error_msg = request.session.pop('user_input_error', None)
         
-    return render(request, "home.html", {'user_input': sentiment})
+    return render(request, "home.html", {'user_input': sentiment, 'user_input_error': error_msg})
